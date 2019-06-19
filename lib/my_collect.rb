c262f8b420a12c96dd69cf6b68aba2e0173f71be
gillianwenhold <gwenhold@mac.com>
2019-06-19 13:14:08 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(list)
+  i = 0
+  collection = []
+  while i < list.length
+    collection << yield(list[i])
+    i += 1
+  end
+  collection
+end
 
+
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(list) {|name| name.split(' ').first}
 
